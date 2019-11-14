df16fb2ef5314c1c39dc88cc229ff09475a3649e
JJ Merelo <jjmerelo@gmail.com>
2019-11-14 20:43:27 +0100

    Incorporates days with transparency to the paper

--- results/plot_results.R
+++ results/plot_results.R
@@ -6,10 +6,10 @@ data$Crossover <- as.factor(data$Crossover)
 data$Population <- as.factor(data$Population)
 data.web <- data[data$Web == "Static",]
 
-ggplot(data.web, aes(x=Population,y=Fitness,color=Crossover,shape=Mutation, size=Copies*10)) + geom_point()+ theme_tufte()+ geom_jitter(width = 0.3, height=0.2)
+ggplot(data.web, aes(x=Population,y=Fitness,color=Crossover,shape=Mutation, size=Copies*10,alpha=Days)) + geom_jitter(width = 0.3, height=0.2)+ theme_tufte()
 ggsave("static.png",width=190, units="mm")
 
 data.juice <- data[data$Web == "Juice Shop",]
 
-ggplot(data.juice, aes(x=Population,y=Fitness,color=Crossover,shape=Mutation, size=Copies*10)) + geom_point()+ theme_tufte()+ geom_jitter(width = 0.3, height=0.2)
+ggplot(data.juice, aes(x=Population,y=Fitness,color=Crossover,shape=Mutation, size=Copies*10,alpha=Days))+ geom_jitter(width = 0.3, height=0.2) +  theme_tufte()
 ggsave("juice-shop.png", width=190, units="mm")
