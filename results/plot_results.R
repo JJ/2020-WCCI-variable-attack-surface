81744e0d1c9dedfa1021ff8808d8c6a7d6baecd7
JJ Merelo <jjmerelo@gmail.com>
2019-11-15 10:37:15 +0100

    Adds time diagram too

--- results/plot_results.R
+++ results/plot_results.R
@@ -2,8 +2,13 @@ library(ggplot2)
 library(ggthemes)
 
 data <- read.csv("all_results.csv")
+
+ggplot(data,aes(x=Population,y=Days,color=Web)) + geom_point()+ theme_tufte()
+
 data$Crossover <- as.factor(data$Crossover)
 data$Population <- as.factor(data$Population)
+
+
 data.web <- data[data$Web == "Static",]
 
 ggplot(data.web, aes(x=Population,y=Fitness,color=Crossover,shape=Mutation, size=Copies*10,alpha=Days)) + geom_jitter(width = 0.3)+ theme_tufte()
@@ -13,3 +18,4 @@ data.juice <- data[data$Web == "Juice Shop",]
 
 ggplot(data.juice, aes(x=Population,y=Fitness,color=Crossover,shape=Mutation, size=Copies*10,alpha=Days))+ geom_jitter(width = 0.3) +  theme_tufte()
 ggsave("juice-shop.png", width=190, units="mm")
+
