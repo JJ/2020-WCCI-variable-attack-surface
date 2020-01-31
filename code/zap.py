7238959e9a9fa5ffb01c1b88501160fd9f8e6667
JJ Merelo <jjmerelo@gmail.com>
2020-01-31 07:57:10 +0100

    Working version (most of the time)

--- code/zap.py
+++ code/zap.py
@@ -10,49 +10,39 @@ import time
 from pprint import pprint
 from zapv2 import ZAPv2
 import sys
-
+# Code from https://github.com/zaproxy/zap-api-python/blob/master/src/examples/basic-spider-scan.py with a free license
 # Change to match the API key set in ZAP, or use None if the API key is disabled
 apikey = None
 
 proxy_host = "http://zap"
 proxy_port = 8080
 proxy = "%s:%s" % (proxy_host, proxy_port)
-
 target = 'http://www.exampletfm.com'
 
+zap = ZAPv2(apikey=apikey, proxies={'http': proxy})
+
+zap.core.new_session(name="abc", overwrite=True)
+# Proxy a request to the target so that ZAP has something to deal with
+print('→ Accessing target {}'.format(target), file=sys.stderr)
+# zap.urlopen(target)
+zap.core.access_url(url=target)
+# Give the sites tree a chance to get updated
+time.sleep(2)
+
+print('Spidering target {}'.format(target), file=sys.stderr)
+# scanid = zap.spider.scan(target, maxchildren="10", recurse="false", subtreeonly="true")
+scanid = zap.spider.scan(target, maxchildren="20")
+# Give the Spider a chance to start
+time.sleep(2)
+while (int(zap.spider.status(scanid)) < 100):
+    # Loop until the spider has finished
+    print('Spider progress %: {}'.format(int(zap.spider.status(scanid))), file=sys.stderr)
+    time.sleep(2)
+
+print('Spider completed', file=sys.stderr)
+zap.spider.stop_all_scans()
 
 def zap_test():
-
-    zap = ZAPv2(apikey=apikey, proxies={'http': proxy})
-
-    zap.core.new_session(name="abc", overwrite=True)
-    # Proxy a request to the target so that ZAP has something to deal with
-    print('Accessing target {}'.format(target), file=sys.stderr)
-    # zap.urlopen(target)
-    zap.core.access_url(url=target)
-    # Give the sites tree a chance to get updated
-    time.sleep(2)
-
-    print('Spidering target {}'.format(target), file=sys.stderr)
-    # scanid = zap.spider.scan(target, maxchildren="10", recurse="false", subtreeonly="true")
-    scanid = zap.spider.scan(target, maxchildren="20")
-    # Give the Spider a chance to start
-    time.sleep(2)
-    while (int(zap.spider.status(scanid)) < 100):
-        # Loop until the spider has finished
-        print('Spider progress %: {}'.format(int(zap.spider.status(scanid))), file=sys.stderr)
-        time.sleep(2)
-
-    print('Spider completed', file=sys.stderr)
-    zap.spider.stop_all_scans()
-
-    # print('Enable all passive scanners -> ' + zap.pscan.enable_all_scanners(), file=sys.stderr)
-
-    # while (int(zap.pscan.records_to_scan) > 0):
-    #     print('Records to passive scan : {}'.format(int(zap.pscan.records_to_scan)), file=sys.stderr)
-    #     time.sleep(2)
-
-    # print('Passive Scan completed', file=sys.stderr)
 
     print('Active Scanning target {}'.format(target), file=sys.stderr)
 
