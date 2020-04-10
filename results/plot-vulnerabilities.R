9a98009e16b1c3f2b019e63886c37a189e72d656
JJ Merelo <jjmerelo@gmail.com>
2020-04-10 17:58:09 +0200

    Plots results

--- results/plot-vulnerabilities.R
+++ results/plot-vulnerabilities.R
@@ -5,5 +5,5 @@ data <- read.csv("vulnerabilities-per-generation-results-02-05-2020.csv")
 data$individual.medium <- data$Medium / data$Population
 data$individual.low <- data$Low / data$Population
 data$Generation <- as.factor(data$Generation)
-data$Population <- as.factor(data$Population)
-ggplot(data,aes(x=Generation,shape=Population))+geom_point(aes(y=individual.medium,color="Medium"))+geom_point(aes(y=individual.low,color="Low"))+theme_tufte()
+ggplot(data[data$Population==16,],aes(x=Generation))+geom_boxplot(aes(y=individual.medium,color="Medium"))+geom_boxplot(aes(y=individual.low,color="Low"))+theme_tufte()
+ggplot(data[data$Population==32,],aes(x=Generation))+geom_boxplot(aes(y=individual.medium,color="Medium"))+geom_boxplot(aes(y=individual.low,color="Low"))+theme_tufte()
