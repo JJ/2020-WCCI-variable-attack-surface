372a2eedc590f5912ac014f77981715f7f051503
JJ Merelo <jjmerelo@gmail.com>
2020-04-11 17:44:35 +0200

    Adds new results

--- results/plot-vulnerabilities.R
+++ results/plot-vulnerabilities.R
@@ -7,3 +7,11 @@ data$individual.low <- data$Low / data$Population
 data$Generation <- as.factor(data$Generation)
 ggplot(data[data$Population==16,],aes(x=Generation))+geom_boxplot(aes(y=individual.medium,color="Medium"))+geom_boxplot(aes(y=individual.low,color="Low"))+theme_tufte()
 ggplot(data[data$Population==32,],aes(x=Generation))+geom_boxplot(aes(y=individual.medium,color="Medium"))+geom_boxplot(aes(y=individual.low,color="Low"))+theme_tufte()
+
+data <- read.csv("secdef-2020.csv")
+data$individual.medium <- data$Medium / data$Population
+data$individual.low <- data$Low / data$Population
+data$Generation <- as.factor(data$Generation)
+ggplot(data[data$Population==16,],aes(x=Generation))+geom_boxplot(aes(y=individual.medium,color="Medium"))+geom_boxplot(aes(y=individual.low,color="Low"))+theme_tufte()
+
+
