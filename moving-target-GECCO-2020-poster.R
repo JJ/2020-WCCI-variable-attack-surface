be980253692102f22fe23507537174bc3392718e
JJ Merelo <jjmerelo@gmail.com>
2020-06-04 12:18:42 +0200

    Evolved poster scripts

--- moving-target-GECCO-2020-poster.R
+++ moving-target-GECCO-2020-poster.R
@@ -8,9 +8,9 @@ suppressPackageStartupMessages({
 ## ----distances, cache=FALSE,echo=FALSE----------------------------------------
 data.md <- read.csv("results/mutual-distances-averages-entropy.csv")
 data.16 = data.md[ data.md$Population == 16, ]
-ggplot(data.16,aes(x=Experiment,y=Avg.Distance))+ geom_boxplot() + theme_tufte()
+ggplot(data.16,aes(x=Experiment,y=Avg.Distance))+ geom_boxplot() + theme_tufte()+theme(axis.text=element_text(size=20), axis.title=element_text(size=24,face="bold"))
 
 
 ## ----entropy, cache=FALSE,echo=FALSE------------------------------------------
-ggplot(data.16,aes(x=Experiment,y=Entropy))+ geom_boxplot() + theme_tufte()
+ggplot(data.16,aes(x=Experiment,y=Entropy))+ geom_boxplot() + theme_tufte()+theme(axis.text=element_text(size=20), axis.title=element_text(size=24,face="bold"))
 
